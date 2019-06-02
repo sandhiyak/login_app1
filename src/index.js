21b4d925c16512cbcefded7a7ac7e99a1b9be4b7
sakthikesavan123 <sakthi@codingmart.com>
2019-06-02 23:00:16 +0530

    check

--- src/index.js
+++ src/index.js
@@ -10,30 +10,92 @@ const styles = {
       textAlign: 'center'
     }
   }
-  
   class Login extends React.Component {
+      
     handleOnSubmit = (e) => {
       e.preventDefault();
       console.log('Submitted!');
     };
+    handleSingUp = (e) => {
+        ReactDOM.render(
+            <Form children={ <Reg /> } />,
+            document.getElementById('root')
+          );
+          
+    };
     render() {
-      return (
-          <form className='form' style={styles.form} onSubmit={this.handleOnSubmit}>
-            <h4>Login</h4>
-            <div className='form-group row'>
-              <input className='input' name="password" type='email' placeholder='Email' required/>
-            </div>
-            <div className='form-group row'>
-              <input className='input' name="password" type='password' placeholder='Password' required/>
-            </div>
-            <div className='form-group row'>
-              <button className='button' type='submit'>Log In</button>
-            </div>
-          </form>
         
-      )
-    }
+
+            return (
+                <div>
+                    <div>
+                        <form className='form' style={styles.form} onSubmit={this.handleOnSubmit}>
+                            <h4>Login</h4>
+                            <div className='form-group row'>
+                            <input className='input' name="email" type='email' placeholder='Email' required/>
+                            </div>
+                            <div className='form-group row'>
+                            <input className='input' name="password" type='password' placeholder='Password' required/>
+                            </div>
+                            <div className='form-group row'>
+                            <button className='button' type='submit'>Log In</button>
+                            </div>
+                            <div className='form-group row'>
+                            <button className='button' type='button' onClick={this.handleSingUp}>Sign Up</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                
+            )
+        }
+       
   }
+
+  class Reg extends React.Component {
+    
+  handleOnSubmit = (e) => {
+    var w= e.preventDefault();
+    console.log(e);
+  };
+  
+  render() {
+      
+
+          return(
+
+            <div>
+            <form className='form' style={styles.form} onSubmit={this.handleOnSubmit}>
+                <h4>Register</h4>
+                <div className='form-group row'>
+                <input className='input' name="f_name" type='text' placeholder='First Name' required/>
+                </div>
+                <div className='form-group row'>
+                <input className='input' name="l_name" type='text' placeholder='Last Name' required/>
+                </div>
+                <div className='form-group row'>
+                <input className='input' name="p_no" type='number' placeholder='Phone No' required/>
+                </div>
+                <div className='form-group row'>
+                <input className='input' name="email" type='email' placeholder='Email' required/>
+                </div>
+                <div className='form-group row'>
+                <input className='input' name="password" type='password' placeholder='Password' required/>
+                </div>
+                <div className='form-group row'>
+                <input className='input' name="c_password" type='password' placeholder='Confirm Password' required/>
+                </div>
+                <div className='form-group row'>
+                <button className='button' type='submit'>Save</button>
+                </div>
+            </form>
+        </div>
+          )
+      }
+  
+}
+
   
   class Form extends React.Component {
     render () {
